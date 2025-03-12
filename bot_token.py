55d3a51cc541c998a377e9770754678732f8f49f
Alexis <144615457+zavulon-lab@users.noreply.github.com>
2025-03-12 20:05:17 +0300

    Update bot_token.py

--- bot_token.py
+++ bot_token.py
@@ -1,1 +1,1 @@
-TOKEN = "MTMyMjMzODAxNzMyNjY2NTgzMw.Gh_Lj4.nmHYwhPiGqKT3Tzrr-daeYfFCFfvAR8B5OPOQ0"  # Замените на ваш токен
+TOKEN = "MTMyMjMzODAxNzMyNjY2NTgzMw.Gg3BT_.ejwVPVWnu-UG0h9wBZDhlmbOlJ2egmV8uwJ-IM"  # Замените на ваш токен
