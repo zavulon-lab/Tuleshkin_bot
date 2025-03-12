f7264a1e07616479a2a4e5e1bf07ee3b04dc3aef
Danila <zzowich@gmal.com>
2025-03-12 21:15:05 +0300

    update

--- bot_token.py
+++ bot_token.py
@@ -1,1 +1,1 @@
-TOKEN = "MTMyMjMzODAxNzMyNjY2NTgzMw.Gg3BT_.ejwVPVWnu-UG0h9wBZDhlmbOlJ2egmV8uwJ-IM"  # Замените на ваш токен
+TOKEN = "MTMyMjMzODAxNzMyNjY2NTgzMw.GDEVpr.Mw5HE6-WW0Y4MYcZOP_usxkLaJDF0K2xetZgxY"  # Замените на ваш токен
